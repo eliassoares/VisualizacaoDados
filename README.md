b8568458593789a98e36ebb97a09595392fdaddb
eliassoares <eliassoares@dcc.ufmg.br>
2016-04-27 20:11:36 -0300

    Possível resposta Pergunta 1

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ Links importantes:
   -Funcionamento da API:  https://github.com/DataViva/dataviva-site/wiki/API
   
   -Data Viva : http://pt.dataviva.info/
+
+
+Descrição da URL:
+	-http://en.dataviva.info/hedu/identificacao_ano/identificacao_regiao/identificacao_universidade/show/
+	
+	-Colocando 'all' no lugar dos identificadores, você pega os identificadores.
--- api.py
+++ api.py
@@ -13,11 +13,44 @@ def retornaJson(link):
 
 		conexoes += 1
 	return None
-	
-if __name__ == '__main__':
+
+#Para cada ano, pega a quantidade de alunos que entraram na faculdade
+# e a quantidade que formaram para cada regiao.
+def pegaAnoRegiao():
+	#descricao da URL:
+	#http://en.dataviva.info/hedu/identificacao_ano/identificacao_regiao/identificacao_universidade/show/
+	#Colocando 'all' no lugar dos identificadores, voce pega os identificadores.
 	linkBase = 'http://en.dataviva.info'
-	pesquisa = '/rais/2012/4mg.show.9/all/all/'
 
-	dicionarioJson = retornaJson(linkBase + pesquisa)
+	dicionarioAnoRegiao = {}
+	for regiao in range(1,6):
+		pesquisa = '/hedu/all/' + str(regiao) + '/all/show/'
+		respostaJson = retornaJson(linkBase + pesquisa)
+		for i in respostaJson['data']:
+			ano = int(i[10])
+			entrants = i[2]
+			graduates = i[1]
+			dicionarioAnoRegiao.setdefault(ano, {}).setdefault(regiao, [0, 0])
+			dicionarioAnoRegiao[ano][regiao][0] += graduates
+			dicionarioAnoRegiao[ano][regiao][1] += entrants
+		
+			
 
-	print dicionarioJson
+
+	return dicionarioAnoRegiao
+
+if __name__ == '__main__':
+
+	d = pegaAnoRegiao()
+	j = json.dumps(d, ensure_ascii=False)
+	
+	with open('pergunta1.json', 'wb') as file:
+		json.dump(j, file)
+
+	'''for i in d:
+		print 'Ano:', str(i)
+		for j in d[i]:
+			print '\tRegiao:',j
+			print '\t\tGraduados:',d[i][j][0],'\n\t\tEntraram:',d[i][j][1], '\n\t\tEntrou - Saiu:',d[i][j][1]-d[i][j][0]
+			'''
+	print j
