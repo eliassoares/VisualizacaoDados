ee90e38475af2deb4f039cde5f6f5aa4e42acf2e
lucasmonteiro001 <lucasmonteiro001@gmail.com>
2016-05-14 13:46:54 -0300

    Conversor e perguntas 3 em csv

---
+++ file_converters/json2csv-p3.py
@@ -0,0 +1,28 @@
+import json
+
+# The structure of the CSV will be:
+# YEAR | CODE | ENTRANT | LEAVING
+
+def main():
+    SRC_FILENAME = 'pergunta3.json'
+
+    with open(SRC_FILENAME, 'r') as fp:
+        collected_data = json.loads(json.load(fp, encoding='utf-8'))
+
+    csv_lines = []
+    # Headers
+    csv_lines.append('Universidade, Ingressantes, Concluintes\n')
+
+    for universidade in collected_data:
+        data = collected_data[universidade]
+        ingressantes = data[0]
+        concluintes = data[1]
+        # print universidade, ingressantes, concluintes
+        csv_lines.append('{},{},{}\n'.format(universidade, ingressantes, concluintes))
+
+    with open('pergunta3.csv', 'w+') as pfp:
+       for line in csv_lines:
+          pfp.write(line)
+
+if __name__ == '__main__':
+    main()
