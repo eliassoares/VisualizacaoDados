0ac0604b0f70202bc6bd15ccd72dbda02d4fa6c6
Gustavo Pereira <ghapereira@gmail.com>
2016-05-27 14:46:42 -0300

    Exemplo de uso de JSON com círculos; TESTE

--- TP2/js/pergunta3.js
+++ TP2/js/pergunta3.js
@@ -4,7 +4,7 @@ var width = 400;
 var barHeight = 20;
 var x = d3.scale.linear()
   .range([0, width]);
-var chart = d3.select("#chartPergunta3")
+var svgContainer = d3.select("#chartPergunta3")
   .attr("width", width);
 
 d3.json("dados/pergunta3.json", function(error, data){
@@ -14,10 +14,50 @@ d3.json("dados/pergunta3.json", function(error, data){
      programa em Python.
      */
   let institutionData = JSON.parse(data);
-  console.log(institutionData);
+
+  // Convert to a table
+  let ingressantes = new Object();
+  let concluintes = new Object();
+
+  for(let inst in institutionData) {
+    ingressantes[inst] = institutionData[inst][0];
+    concluintes[inst] = institutionData[inst][1];
+  }
 
   x.domain([0, d3.max(institutionData, function(d) {return d.value;})]);
-  chart.attr("height", barHeight * Object.keys(institutionData).length);
+  svgContainer.attr("height", barHeight * Object.keys(institutionData).length);
+
+  let bars = svgContainer.selectAll("g")
+              .data(ingressantes)
+              .enter()
+              .append("g");
+
+  bars.append("rect")
+    .attr("x", function(d) {return x(d.value); })
+    .attr("height", barHeight - 1);
+
+  bars.append("text")
+    .attr("x", function(d) {return x(d.value) - 3; })
+    .attr("y", barHeight / 2)
+    .attr("dy", ".35em")
+    .text(function(d) {return d.value;});
+
+  // Exemplo de JSON com círculos
+  var jsonCircles = [
+    { "x_axis": 30, "y_axis": 30, "radius": 20, "color" : "green" },
+    { "x_axis": 70, "y_axis": 70, "radius": 20, "color" : "purple"},
+    { "x_axis": 110, "y_axis": 100, "radius": 20, "color" : "red"}];
+
+  var circles = svgContainer.selectAll("circle")
+                            .data(jsonCircles)
+                            .enter()
+                            .append("circle");
+
+  var circleAttributes = circles
+                           .attr("cx", function (d) { return d.x_axis; })
+                           .attr("cy", function (d) { return d.y_axis; })
+                           .attr("r", function (d) { return d.radius; })
+                           .style("fill", function(d) { return d.color; });
 });
 }());
 
