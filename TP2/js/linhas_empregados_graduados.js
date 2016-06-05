0ee3e77cd4109c584421283a33940cfb92d103c0
Elias Soares <eliassoares@dcc.ufmg.br>
2016-06-04 23:47:30 -0300

    Melhorando Gráfico1

--- TP2/js/linhas_empregados_graduados.js
+++ TP2/js/linhas_empregados_graduados.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
 // definicao do posicionamento do grafico d;
-var margin = {top: 20, right: 195, bottom: 30, left: 65},
+var margin = {top: 20, right: 270, bottom: 30, left: 65},
     width = 560 - margin.left - margin.right,
     height = 380 - margin.top - margin.bottom;
 
@@ -29,7 +29,7 @@ var yAxis = d3.svg.axis()  // para o eixo y pega a scala e o posicionamento;
 
 var line = d3.svg.line()    // é o que desenha a linha;
     .interpolate("basis")   // tipo de interpolacao;
-    .x(function(d) { return x(d.ano); }) // retornar os pontos de x;
+    .x(function(d) { return x(d.Ano); }) // retornar os pontos de x;
     .y(function(d) { return y(d.pessoas); }); // retornar os pontos de y;
 
 var svg = d3.select("#chartEmpregadosGraduadosAno").append("svg")  // para onde vai no index.html,
@@ -52,23 +52,23 @@ d3.csv("./dados/graduados_empregados.csv", function(error, data){
   if(error)
   throw error;
 
- color.domain(d3.keys(data[0]).filter(function(key) { return key !== "ano"; })); // especifico para o grafico de linhas dominio de cores da var color;
+ color.domain(d3.keys(data[0]).filter(function(key) { return key !== "Ano"; })); // especifico para o grafico de linhas dominio de cores da var color;
 
 
   data.forEach(function(d) {      // for para transformar o ano e apresentar somente o ano - gambiarra transformando de 2009 - 2013 para plotar apenas isso;
-    d.ano = parseano( d.ano.toString() );
+    d.Ano = parseano( d.Ano.toString() );
   });
 
   var dados = color.domain().map(function(name) {
     return {
       name: name,
       values: data.map(function(d) {
-        return {ano: d.ano, pessoas: +d[name]};
+        return {Ano: d.Ano, pessoas: +d[name]};
       })
     };
   });
 
-  x.domain(d3.extent(data, function(d) { return d.ano; })); // dominio de x
+  x.domain(d3.extent(data, function(d) { return d.Ano; })); // dominio de x
   y.domain([ // dominio de y
     d3.min(dados, function(c) { return d3.min(c.values, function(v) { return v.pessoas; }); }), // veio do exemplo que peguei fazia uma transformacao para temperatura eu tirei e ñão funcionou entao deixei assim
     d3.max(dados, function(c) { return d3.max(c.values, function(v) { return v.pessoas; }); })
@@ -86,6 +86,7 @@ d3.csv("./dados/graduados_empregados.csv", function(error, data){
       .attr("transform", "rotate(-90)") // para colocar a escrita  Estudantes ou Vagas na vertical 
       .attr("y", 4) // distancia para a escrita
       .attr("dy", ".71em") // deslocamento para a escrita
+	.attr("dx", "-4em")
       .style("text-anchor", "end")
       .text("Estudantes ou Vagas");
 
@@ -101,15 +102,17 @@ d3.csv("./dados/graduados_empregados.csv", function(error, data){
 
   grupodados.append("text") // pendurar os nomes de cada linha;
       .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
-      .attr("transform", function(d) { return "translate(" + x(d.value.ano) + "," + y(d.value.pessoas) + ")"; }) // o que vai ser escrito mais posicionamento
+      .attr("transform", function(d) { return "translate(" + x(d.value.Ano) + "," + y(d.value.pessoas) + ")"; }) // o que vai ser escrito mais posicionamento
       .attr("x", 4) 
       .attr("y", function(d){  // para o posicionmamento do texto em y 
-        if ( d.name === "diferenca_graduados_jobs") return 12; // gambiarra porque dois texto ficaram na mesma posicao no grafico
+        if ( d.name === "Diferença Graduados Empregos") return 12; // gambiarra porque dois texto ficaram na mesma posicao no grafico
         else 0;          // se for legenda diferenca_graduandos vai retornar para a posicao y +12(valor que ficou bom) se nao é o fim da linha mesmo; 
                          // se nao é +0 e vai para fim da linha a legenda m         
       })
       .attr("dy", ".20em")
       .text(function(d) { return d.name; });
+	
+	grupodados.selectAll("text")
 });
 
 }); // fim on ready
